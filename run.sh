6f6eee0e07343732bd1cdd85bbc4203b2d1210c7
Christian Lenz <christian.lenz@doksafe.de>
2023-04-06 19:00:02 +0200

    Try to install a lot docker packages

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ docker --version > /dev/null || EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
     # for debian or ubuntu
     # apt -y install podman
-    apt -y install docker
+    apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # for alpine
     # apk add podman
